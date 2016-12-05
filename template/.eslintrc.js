9d3276d5992e880298e533075d1ca7697a982f82
xfcf1 <xfcf@163.com>
2016-12-05 11:40:13 +0800

    Update .eslintrc.js

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -25,6 +25,6 @@ module.exports = {
     ],
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': 0
   }
 };
