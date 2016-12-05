350081459afb93fcfb3e0e75c7e496eeb0b677ec
alex <xiaohao.dai@ele.me>
2016-12-05 11:28:13 +0800

    modify  style code

--- template/src/components/Test/Test.js
+++ template/src/components/Test/Test.js
@@ -10,8 +10,8 @@ class Test extends Component {
     return (
       <div className={ styles.red }>
         <h1>this is router test page</h1>
-        <h2>{text}<span style={{ color: 'blue' }}>{`${className}`}</span></h2>
-        <span style={{ fontSize: '18pt' }}><Link to='/'>link back to home page</Link></span>
+        <h2>{text}<span>{`${className}`}</span></h2>
+        <span><Link to='/'>link back to home page</Link></span>
       </div>
     )
   }
