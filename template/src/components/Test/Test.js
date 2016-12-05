ee746bd0e01543a10114b24ab69c963823c835fb
xfcf1 <xfcf@163.com>
2016-12-05 11:23:33 +0800

    Update Test.js

--- template/src/components/Test/Test.js
+++ template/src/components/Test/Test.js
@@ -10,7 +10,7 @@ class Test extends Component {
     return (
       <div className={ styles.red }>
         <h1>this is router test page</h1>
-        <h2>{text}<span style={{ color: 'blue' }}>{`${className}`}</span></h2>
+        <h2>{text}<span>{`${className}`}</span></h2>
         <span style={{ fontSize: '18pt' }}><Link to='/'>link back to home page</Link></span>
       </div>
     )
